145e0f95072e9ca28e8f54b514f415ecf9e3c059
Marcos <marrodoso@alum.us.es>
2022-04-06 19:05:50 +0200

    heroku

--- src/main/java/medicaldate/MedicaldateApplication.java
+++ src/main/java/medicaldate/MedicaldateApplication.java
@@ -6,15 +6,26 @@ import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MedicaldateApplication {
+public class MedicaldateApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedicaldateApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(MedicaldateApplication.class, args);
+//	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(MedicaldateApplication .class);
+    }
+
+    public static void main(String[] args) {
+         SpringApplication.run(MedicaldateApplication .class, args);
+    }
 	
 //	 @Bean
 //	    ServletRegistrationBean jsfServletRegistration (ServletContext servletContext) {
