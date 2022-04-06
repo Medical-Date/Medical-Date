c891fbb02616089d8e9c900d898ead67d371fcb0
Marcos <marrodoso@alum.us.es>
2022-04-06 18:25:15 +0200

    heroku

---
+++ src/main/java/medicaldate/heroku/HomeController.java
@@ -0,0 +1,12 @@
+package medicaldate.heroku;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "Hola mundo.";
+	}
+}
